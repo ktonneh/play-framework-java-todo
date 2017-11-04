4103e853b59abbf17f29c7b52356fd4b3ac24ec1
ktonneh <ktonneh@gmail.com>
2017-11-04 13:06:00 +0300

    login functon fixed

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -24,12 +24,26 @@ public class Application extends Controller {
 	public static Result doLogin(){
         DynamicForm form= Form.form().bindFromRequest();
         Logger.info(form.get("username"));
+        ObjectNode result = Json.newObject();
 
-        ObjectNode result = null;
-        result = Json.newObject();
+        String username=form.get("username");
+        String password=form.get("password");
+
+        UserVO userVO=UserVO.findByUsername(username);
+        if (userVO!=null){
+            //check password
+            if (!userVO.password.equals(password)){
+                result.put("code","201");
+                result.put("message","Invalid Password!!");
+                return ok(result);
+            }
+        } else {
+            result.put("code","201");
+            result.put("message","Invalid Username!!");
+            return ok(result);
+        }
         result.put("200","Success");
         result.put("message","Login Success!!");
-
 	    return ok(result);
     }
 
@@ -41,16 +55,24 @@ public class Application extends Controller {
 	    String password = staney.get("password");
 	    //String username = frm.get("username");
 
+//        Logger.info("");
+        //check if username exists
+        if (UserVO.findByUsername(username) !=null){
+            majibu.put("message","User "+ username +" already exists !!");
+            majibu.put("code","201");
+            return  ok(majibu);
+        }
+
         UserVO userVO=new UserVO();
-        userVO.username = username;
+        userVO.username = username.toLowerCase();
         userVO.password = password;
         userVO.save();
         //Ebean.save(userVO);
 
         Logger.info("Username"+username);
-
         majibu.put("message","User "+ username +" created successfully! Yeey");
         majibu.put("code","200");
+
 	    return ok(majibu);
     }
 	
